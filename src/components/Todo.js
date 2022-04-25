16c03ff4d967c69f5f6b0838b5770a21c7f19572
Subham Gupta <99577900+0subham1@users.noreply.github.com>
2022-04-25 12:46:58 +0530

    todo1

---
+++ src/components/Todo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <>
+    <div className='main-div'>
+      <div className='child-div'>
+    <figure>
+      <figcaption>
+        add here
+      </figcaption>
+    </figure>
+
+    <div className='addItems'>
+      <input type="text" placeholder='enter here'>
+      </input>
+          <button id='addBtn'>+</button>
+      <i></i>
+    </div>
+ 
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Todo
